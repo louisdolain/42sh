2c18dcbb1356cd76bb5c1e5105f51d3f3b1f5227
KATARY Maryse <maryse.katary@epitech.eu>
2024-04-30 13:46:32 +0200

    feat:Create a .h for globbing

---
+++ include/globbing.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** B-PSU-200-PAR-2-1-42sh-pierre.lissope
+** File description:
+** glob.h
+*/
+
+#pragma once
+
+#include <glob.h>
+#include <string.h>
+
+size_t nbr_wdr(const char *str);
+int handle_globbing(char *cmd, char *line);
+int contains_globbing_pattern(const char *str);
