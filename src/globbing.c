2eeadaead2239566546b1d9b54c76a3ffb1eaccb
KATARY Maryse <maryse.katary@epitech.eu>
2024-05-03 17:03:37 +0200

    feat:refactor all globbing code

--- src/globbing.c
+++ src/globbing.c
@@ -28,47 +28,30 @@ size_t nbr_wdr(const char *str)
     return count;
 }
 
-static int execute_command(char *cmd, char **arg)
-{
-    pid_t pid = fork();
-
-    if (pid == -1) {
-        perror("fork");
-        return -1;
-    } else if (pid == 0) {
-        execvp(cmd, arg);
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } else {
-        waitpid(pid, NULL, 0);
-    }
-    return 0;
-}
-
 int contains_globbing_pattern(const char *str)
 {
     return strchr(str, '*') != NULL || strchr(str, '?') != NULL
         || strchr(str, '[') != NULL;
 }
 
-int handle_globbing(char *cmd, char *line)
+int handle_globbing(char *cmd, char **parsed_input,
+    char **paths, char ***env)
 {
     glob_t globbuf;
-    size_t nbr_bfr_glob = nbr_wdr(line);
-    char **arg = NULL;
+    size_t nbr_bfr_glob = nbr_wdr(cmd);
     int i = 0;
 
-    arg = my_str_to_all_array(cmd, " ");
     globbuf.gl_offs = nbr_bfr_glob;
-    while (arg[i]) {
-        glob(arg[i], GLOB_DOOFFS | (i == 0 ? 0 : GLOB_APPEND), NULL, &globbuf);
+    while (parsed_input[i]) {
+        glob(parsed_input[i], GLOB_DOOFFS |
+            (i == 0 ? 0 : GLOB_APPEND), NULL, &globbuf);
         i++;
     }
     for (int j = 0; j < i; j++) {
-        if (contains_globbing_pattern(arg[j]))
+        if (contains_globbing_pattern(parsed_input[j]))
             continue;
-        globbuf.gl_pathv[j] = arg[j];
+        globbuf.gl_pathv[j] = parsed_input[j];
     }
-    execute_command(line, globbuf.gl_pathv);
+    exec_it(globbuf.gl_pathv, paths, env);
     return 0;
 }
