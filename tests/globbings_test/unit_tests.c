b973356c3bc4b9a8560a0bde2a8fb75f37ac38db
KATARY Maryse <maryse.katary@epitech.eu>
2024-05-06 14:41:12 +0200

    feat:correction of some coding style

--- tests/globbings_test/unit_tests.c
+++ tests/globbings_test/unit_tests.c
@@ -11,48 +11,48 @@ void redirect_all_std(void)
 
 Test(nbr_wdr, test_1, .init = redirect_all_std)
 {
-    const char* str = "Hello world";
+    const char *str = "Hello world";
     cr_assert_eq(nbr_wdr(str), 2);
 }
 
 Test(nbr_wdr, test_2, .init = redirect_all_std)
 {
-    const char* str = "HelloWorld";
+    const char *str = "HelloWorld";
     cr_assert_eq(nbr_wdr(str), 1);
 }
 
 Test(nbr_wdr, test_3, .init = redirect_all_std)
 {
-    const char* str = "";
+    const char *str = "";
     cr_assert_eq(nbr_wdr(str), 0);
 }
 
 Test(nbr_wdr, test_4, .init = redirect_all_std)
 {
-    const char* str = "Hello   World";
+    const char *str = "Hello   World";
     cr_assert_eq(nbr_wdr(str), 2);
 }
 
 Test(contains_globbing_pattern, test_5, .init = redirect_all_std)
 {
-    const char* str = "Hello*";
+    const char *str = "Hello*";
     cr_assert(contains_globbing_pattern(str));
 }
 
 Test(contains_globbing_pattern, test_6, .init = redirect_all_std)
 {
-    const char* str = "HelloWorld";
+    const char *str = "HelloWorld";
     cr_assert_not(contains_globbing_pattern(str));
 }
 
 Test(contains_globbing_pattern, test_7, .init = redirect_all_std)
 {
-    const char* str = "*";
+    const char *str = "*";
     cr_assert(contains_globbing_pattern(str));
 }
 
 Test(contains_globbing_pattern, test_8, .init = redirect_all_std)
 {
-    const char* str = "*?[";
+    const char *str = "*?[";
     cr_assert(contains_globbing_pattern(str));
 }
