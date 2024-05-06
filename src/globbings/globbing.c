6103e45b0c5e39e5171d3a39347bb0b19339d0f4
KATARY Maryse <maryse.katary@epitech.eu>
2024-05-07 00:21:17 +0200

    fix: remove function to handle units_tests

---
+++ src/globbings/globbing.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2023
+** B-PSU-200-PAR-2-1-42sh-pierre.lissope
+** File description:
+** globbing.c
+*/
+
+#include "my.h"
+#include "globbing.h"
+
+size_t nbr_wdr(const char *str)
+{
+    int count = 0;
+    int in_word = 0;
+
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            in_word = 0;
+        }
+        if (str[i] == '*' || str[i] == '?' || str[i] == '[') {
+            break;
+        }
+        if (!in_word && str[i] != ' ') {
+            in_word = 1;
+            count++;
+        }
+    }
+    return count;
+}
+
+int contains_globbing_pattern(const char *str)
+{
+    return strchr(str, '*') != NULL || strchr(str, '?') != NULL
+        || strchr(str, '[') != NULL;
+}
