d12d3f758a37779336dfe6fc06e6a1321e11aa74
KATARY Maryse <maryse.katary@epitech.eu>
2024-05-07 17:20:36 +0200

    style: handle coding style

--- src/globbings/exec_globbings.c
+++ src/globbings/exec_globbings.c
@@ -7,6 +7,16 @@
 #include "my.h"
 #include "globbing.h"
 
+int check_glob(char **parsed_input, glob_t globbuf, int i)
+{
+    for (int j = 0; j < i; j++) {
+        if (contains_globbing_pattern(parsed_input[j]))
+            continue;
+        globbuf.gl_pathv[j] = parsed_input[j];
+    }
+    return 0;
+}
+
 int handle_globbing(char *cmd, char **parsed_input,
     char **paths, char ***env)
 {
@@ -14,17 +24,18 @@ int handle_globbing(char *cmd, char **parsed_input,
     size_t nbr_bfr_glob = nbr_wdr(cmd);
     int i = 0;
 
-    globbuf.gl_offs = nbr_bfr_glob;
+    globbuf.gl_offs = 1;
     while (parsed_input[i]) {
-        glob(parsed_input[i], GLOB_DOOFFS | GLOB_TILDE_CHECK | GLOB_MARK |
+        glob(parsed_input[i], GLOB_DOOFFS |
             (i == 0 ? 0 : GLOB_APPEND), NULL, &globbuf);
         i++;
     }
-    for (int j = 0; j < i; j++) {
-        if (contains_globbing_pattern(parsed_input[j]))
-            continue;
-        globbuf.gl_pathv[j] = parsed_input[j];
+    if (globbuf.gl_pathc == 0) {
+        printf("Aucun fichier trouvé avec le motif donné.\n");
+        globfree(&globbuf);
+        return -1;
     }
+    check_glob(parsed_input, globbuf, i);
     exec_cmd(globbuf.gl_pathv, paths, env);
     return 0;
 }
