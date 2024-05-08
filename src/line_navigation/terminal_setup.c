16f8233346222c2bb897949bf5a1f7993e5c2398
Mariia Semenchenko <mariia.semenchenko@epitech.eu>
2024-05-08 18:31:52 +0200

    fix: fixed valgrind errors

--- src/line_navigation/terminal_setup.c
+++ src/line_navigation/terminal_setup.c
@@ -7,9 +7,12 @@
 #include "navigation.h"
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
 
 void set_terminal_mode(struct termios *old_rules, struct termios *new_rules)
 {
+    memset(old_rules, 0, sizeof(struct termios));
+    memset(new_rules, 0, sizeof(struct termios));
     tcgetattr(STDIN_FILENO, old_rules);
     *new_rules = *old_rules;
     new_rules->c_lflag &= ~ICANON;
